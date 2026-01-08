a7568c90b2f48da4a404ca228a7b7eefbcec6ec2
premkumargunasekar <124994106+premkumargunasekar@users.noreply.github.com>
2026-01-08 11:13:30 +0530

    Update gcp_auto_expand.py

--- library/gcp_auto_expand.py
+++ library/gcp_auto_expand.py
@@ -6,7 +6,7 @@ import re
 import ipaddress
 import subprocess
 
-UTIL_THRESHOLD = 30        # % threshold
+UTIL_THRESHOLD = 70        # % threshold
 BASE_PREFIX = 16           # Base pool prefix
 
 
@@ -191,3 +191,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
